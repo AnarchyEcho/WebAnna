0c8331682973621391ae01a259dd15937b9852f1
AnarchyEcho <anarchytrex@hotmail.com>
2024-05-03 06:54:28 +0200

    fr this time

--- utils/saveConfig.ts
+++ utils/saveConfig.ts
@@ -1,5 +1,8 @@
+import type { IGuild } from '~/interfaces';
+
 export const saveConfig = async (hookUrl: string) => {
   const fullConfig = useState('fullConfig');
+  const chosenGuild = useState<IGuild>('chosenGuild');
   const msgObject = useState<{
     title: string,
     id: string
@@ -7,7 +10,7 @@ export const saveConfig = async (hookUrl: string) => {
   const saving = useState<boolean>('saving', () => false);
   const formdata = new FormData();
   formdata.append('file', new Blob([JSON.stringify(fullConfig.value)], { type: 'application/json' }), msgObject.value.title.replace('_file', '.json'));
-  formdata.append('payload_json', JSON.stringify({ content: `${msgObject.value.title} ${msgObject.value.id}` }));
+  formdata.append('payload_json', JSON.stringify({ content: `${msgObject.value.title} ${chosenGuild.value.id}` }));
   saving.value = true;
   await fetch(`${hookUrl}/messages/${msgObject.value.id}`, {
     method: 'PATCH',
