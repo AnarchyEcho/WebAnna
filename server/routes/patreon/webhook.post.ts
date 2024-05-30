33153ebf686c4ba3c4b72153ffa9cc09ead2a9b5
AnarchyEcho <anarchytrex@hotmail.com>
2024-05-30 16:10:10 +0200

    should post to webhook

--- server/routes/patreon/webhook.post.ts
+++ server/routes/patreon/webhook.post.ts
@@ -1,5 +1,14 @@
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
-  console.log(body);
+  // https://discord.com/api/webhooks/1232738440949268501/nWYyCWB-nf_f6GeA9rlFOXRT54t2wsu7Q57TUXuA5_RnCHH7ga1i2h7vxdUOZX6_nLX5
+  await fetch('https://discord.com/api/webhooks/1232738440949268501/nWYyCWB-nf_f6GeA9rlFOXRT54t2wsu7Q57TUXuA5_RnCHH7ga1i2h7vxdUOZX6_nLX5', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      content: body,
+    }),
+  }).then(x => x.json()).then(console.log);
   return body;
 });
